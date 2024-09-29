806083addb7876f92cbdb789714509bf1aed7d0a
Luc Van Minh <luk.mink@gmail.com>
2024-09-30 00:02:49 +0700

    update refresh token

--- credentials/LarksuiteTenantApi.credentials.ts
+++ credentials/LarksuiteTenantApi.credentials.ts
@@ -1,18 +1,33 @@
 import type {
-	IAuthenticateGeneric,
+	// IAuthenticateGeneric,
 	ICredentialDataDecryptedObject,
 	ICredentialTestRequest,
 	ICredentialType,
 	IHttpRequestHelper,
+	IHttpRequestOptions,
 	INodeProperties,
 } from 'n8n-workflow';
+
+type ILarkSuiteCredentials = {
+	appId: string;
+	appSecret: string;
+	tenantAccessToken: string;
+};
+
+// temporary workaround to store the token expiration
+// by default, each token will be expired in 2 hours
+// so we need to store the token expiration time
+const tokenExpiration = 2 * 60 * 60 * 1000;
+
+const tokenExpirationMap = new Map<string, number>();
 
 export class LarkSuiteTenantApi implements ICredentialType {
 	name = 'larkSuiteTenantApi';
 
 	displayName = 'LarkSuite Tenant';
 
-	documentationUrl = 'https://open.larksuite.com/document/server-docs/getting-started/api-access-token/g';
+	documentationUrl =
+		'https://open.larksuite.com/document/server-docs/getting-started/api-access-token/g';
 
 	properties: INodeProperties[] = [
 		{
@@ -35,9 +50,65 @@ export class LarkSuiteTenantApi implements ICredentialType {
 			default: '',
 			typeOptions: {
 				expirable: true,
+			},
+		},
+	];
+
+	isValidToken(token: string) {
+		if (!token) {
+			return false;
+		}
+
+		if (!tokenExpirationMap.has(token)) {
+			return false
+		}
+
+		if (tokenExpirationMap.has(token)) {
+			const expiration = tokenExpirationMap.get(token);
+			if (expiration && expiration < Date.now()) {
+				return false;
 			}
 		}
-	];
+
+		return true;
+	}
+
+	async authenticate(
+		credentials: ICredentialDataDecryptedObject,
+		requestOptions: IHttpRequestOptions,
+	): Promise<IHttpRequestOptions> {
+		//check whether the token is an API Key or an access token
+		const { tenantAccessToken } = credentials as ILarkSuiteCredentials;
+
+		// @ts-ignore
+		console.log('tenantAccessToken', tenantAccessToken);
+		// Workaround to check whether the token is expired or not
+		// Since the api not returning 401 due to invalid re-authentication handling behavior by n8n
+		// Then we need to check the token expiration manually
+		// @ts-ignore
+		if (!this.isValidToken(tenantAccessToken)) {
+			// manually throw a mock 401 axios error
+			// @ts-ignore
+			console.log('Token is expired');
+			throw {
+				response: {
+					status: 401,
+					statusText: 'Unauthorized',
+					data: {
+						code: 401,
+						msg: 'Unauthorized',
+					},
+				},
+			};
+		}
+
+		// set the Authorization header
+		if (requestOptions.headers) {
+			requestOptions.headers['Authorization'] = `Bearer ${tenantAccessToken}`;
+		}
+
+		return requestOptions;
+	}
 
 	async preAuthentication(this: IHttpRequestHelper, credentials: ICredentialDataDecryptedObject) {
 		const { tenant_access_token } = (await this.helpers.httpRequest({
@@ -51,23 +122,45 @@ export class LarkSuiteTenantApi implements ICredentialType {
 				'Content-Type': 'application/json',
 			},
 		})) as { tenant_access_token: string };
-		return { tenantAccessToken: tenant_access_token };
+
+		// trim the token \s, \n, \r
+		// t-g2069savYQEQMUPZBDUXN2JBSHAID6SAX5XUBS2F\r\n
+		const tenantAccessToken = tenant_access_token.replace(/[\s\n\r]/g, '');
+
+		// store the token expiration
+		tokenExpirationMap.set(tenantAccessToken, Date.now() + tokenExpiration);
+
+		// store up to 100 tokens
+		if (tokenExpirationMap.size > 100) {
+			tokenExpirationMap.delete(tokenExpirationMap.keys().next().value);
+		}
+
+		return { tenantAccessToken };
 	}
 
-	authenticate: IAuthenticateGeneric = {
-		type: 'generic',
-		properties: {
-			headers: {
-				Authorization: '=Bearer {{$credentials.tenantAccessToken}}',
-			},
-		},
-	};
+	// authenticate: IAuthenticateGeneric = {
+	// 	type: 'generic',
+	// 	properties: {
+	// 		headers: {
+	// 			Authorization: '=Bearer {{$credentials.tenantAccessToken}}',
+	// 		},
+	// 	},
+	// };
 
 	test: ICredentialTestRequest = {
 		request: {
 			method: 'GET',
 			url: 'https://open.larksuite.com/open-apis/tenant/v2/tenant/query',
-		}
-	}
-
+		},
+		rules: [
+			{
+				errorMessage: 'Authorization Error: Make sure to use the correct App ID and App Secret',
+				type: 'responseCode',
+				properties: {
+					value: 200,
+					message: '',
+				},
+			},
+		],
+	};
 }
